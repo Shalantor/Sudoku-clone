9fdda5412010f2dc18fbf49900a0da091d2bbb9b
shalantor <gkaraolanis@uth.gr>
2016-04-03 22:30:15 +0300

    Added top Label for Menu

--- UserInterface.java
+++ UserInterface.java
@@ -1,12 +1,27 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.*;
 
 public class UserInterface{
 
     public UserInterface(int width,int height){
+
+        /*Creating window and setting options for it*/
+
         JFrame sudoku = new JFrame("SudokuGK");
         sudoku.setSize(width,height);
         sudoku.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         sudoku.setVisible(true);
+        sudoku.setLayout(new BorderLayout());
+
+        /*creating labels*/
+
+        JLabel title = new JLabel("New_Game");
+        title.setBorder(BorderFactory.createMatteBorder(0,0,2,0,Color.blue));
+        sudoku.add(title,BorderLayout.NORTH);
+
+        JLabel gameGrid = new JLabel();
+        sudoku.add(gameGrid,BorderLayout.CENTER);
     }
 
 }
